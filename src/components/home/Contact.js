9c5752eee716de81df10e46928085c5ca43646c3
Lu, Jun <luj1985@gmail.com>
2017-02-04 00:48:16 +0800

    update contact form

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -5,7 +5,7 @@ require('./Contact.scss')
 class Contact extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
-    // console.log(e.target);
+    window.alert('send site message');
   }
   render() {
     return (
@@ -13,9 +13,10 @@ class Contact extends React.Component {
         <h3>{this.props.title}</h3>
         <div id="site-message">
           <form onSubmit={this.handleSubmit.bind(this)}>
-            <input type="text" placeholder="Title" />
-            <textarea rows="10" placeholder="Submit your content here" />
-            <button type="submit">Submit</button>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <textarea rows="10" placeholder="Message" />
+            <button type="submit">Send Message</button>
           </form>
         </div>
       </section>
